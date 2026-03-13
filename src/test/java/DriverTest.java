54c79114c4c3d95d4c9932e79bd666b5345e36c1
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2026-03-13 14:44:13 +0100

    Ajout des test pour la fonction is adult

--- src/test/java/DriverTest.java
+++ src/test/java/DriverTest.java
@@ -4,16 +4,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DriverTest {
 
-    private Driver driver;
+    private Driver driver1;
+    private Driver driver2;
+    private Driver driver3;
 
     @BeforeEach
     public void setUp() {
-        driver = new Driver("John", 20);
+        driver1 = new Driver("John", 20);
+        driver2 = new Driver("Theo", 9);
+        driver3 = new Driver("Alice", 10);
     }
 
     @Test
     public void testIsAdult() {
-        assertThat(driver.isAdult()).isTrue();
+        assertThat(driver1.isAdult()).isTrue();
+    }
+
+    @Test
+    public void testIsNotAdult(){
+        assertThat(driver2.isAdult()).isFalse();
+    }
+
+    @Test void testIsAdultBoundary() {
+        assertThat(driver3.isAdult()).isTrue();
     }
     
 }
